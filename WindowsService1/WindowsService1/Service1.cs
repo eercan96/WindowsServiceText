a6ee79eeac2bb9bcb75df955660ddfbd32ae40c6
Emre <emreercan755@gmail.com>
2025-04-17 21:41:29 +0300

    writing to text file

--- WindowsService1/WindowsService1/Service1.cs
+++ WindowsService1/WindowsService1/Service1.cs
@@ -3,15 +3,22 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
+using System.Diagnostics.Eventing.Reader;
+using System.IO;
 using System.Linq;
 using System.ServiceProcess;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace WindowsService1
 {
+    
     public partial class Service1 : ServiceBase
     {
+        System.Timers.Timer timer = new System.Timers.Timer();  
+
         public Service1()
         {
             InitializeComponent();
@@ -19,10 +26,43 @@ namespace WindowsService1
 
         protected override void OnStart(string[] args)
         {
+            WriteToFile("Service work started" + DateTime.Now);
+            timer.Elapsed += new ElapsedEventHandler(OnElapsedTime);
+            timer.Interval=5000;
+            timer.Enabled=true;
         }
 
         protected override void OnStop()
         {
+            WriteToFile("Service Stop" + DateTime.Now);
+        }
+        private void OnElapsedTime(object sender, ElapsedEventArgs e) 
+        {
+            WriteToFile("Service continues to work" + DateTime.Now);
+        }
+
+        public void WriteToFile (string message)
+        {
+            string directoryPath = AppDomain.CurrentDomain.BaseDirectory+"/Logs";
+            if (!Directory.Exists(directoryPath))
+            {
+                Directory.CreateDirectory(directoryPath);
+            }
+            string textPath = AppDomain.CurrentDomain.BaseDirectory + "/Logs/myservice.txt";
+            if (!File.Exists(textPath))
+            {
+                using (StreamWriter sw = File.CreateText(textPath))
+                {
+                    sw.WriteLine(message);
+                }
+            }
+            else 
+            {
+                using(StreamWriter sw = File.CreateText(textPath))
+                {
+                    sw.WriteLine(message);
+                }
+            }
         }
     }
 }
